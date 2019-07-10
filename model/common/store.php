4acd492d73d9173c78d99cca820a889c11d971b8
Alexandrvn <php4.drvn@gmail.com>
2019-07-10 13:39:51 +0300

    Fix bug

--- model/common/store.php
+++ model/common/store.php
@@ -19,7 +19,7 @@ class ModelCommonStore extends Model
         $sql->from('store', 's');
         if (_PS_VERSION_ > '1.7.0.0') {
             $sql->leftJoin('store_lang', 'sl', 'sl.`id_store` = s.`id_store`');
-            $sql->where('cl.`id_lang` = ' . (int) $this->context->language->id);
+            $sql->where('sl.`id_lang` = ' . (int) $this->context->language->id);
         }
         $sql->where('s.`active` = 1');
 
